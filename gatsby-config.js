4987540225eb6c40d2bf56d2df092ab13fe3df7a
Edith Emmings <emmings.edith@gmail.com>
2019-11-03 10:52:47 -0600

    site metadata update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter - Strata by HTML5 UP",
-    author: "Hunter Chang",
-    description: "A Gatsby.js Starter based on Strata by HTML5 UP"
+    title: "Edith Emmings",
+    author: "Edith Emmings",
+    description: "Edith Emmings Software Engineer Portfolio"
   },
   plugins: [
     'gatsby-plugin-react-helmet',
