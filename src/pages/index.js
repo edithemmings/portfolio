47cb56a9f8298dd4286be96da4ae9bb60d0315da
Edith Emmings <emmings.edith@gmail.com>
2019-11-03 18:03:03 -0600

    bg image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Helmet from 'react-helmet'
+import nodemailer from 'nodemailer'
 
 import Layout from '../components/layout'
 // import Lightbox from 'react-images'
@@ -23,7 +24,48 @@ const DEFAULT_IMAGES = [
 ];
 
 class HomeIndex extends React.Component {
+    handleChange = (event, keyName) => {
+        this.setState({
+            ...this.state,
+            [keyName]: event.target.value
+        })
+    }
+    handleEmailSubmit = () => {
+        async function main() {
+            // Generate test SMTP service account from ethereal.email
+            // Only needed if you don't have a real mail account for testing
+            let testAccount = await nodemailer.createTestAccount();
 
+            // create reusable transporter object using the default SMTP transport
+            let transporter = nodemailer.createTransport({
+                host: 'smtp.gmail.com',
+                port: 587,
+                secure: false, // true for 465, false for other ports
+                auth: {
+                    user: process.env.EMAIL_ADDRESS, // generated ethereal user
+                    pass: process.env.EMAIL_PASSWORD // generated ethereal password
+                }
+            });
+
+            // send mail with defined transport object
+            let info = await transporter.sendMail({
+                from: '"Fred Foo 👻" <edieb.543@gmail.com>', // sender address
+                to: 'emmings.edith@gmail.com', // list of receivers
+                subject: `NEW MESSAGE from ${this.state.name}`, // Subject line
+                text: `EMAIL: ${this.state.email}`, // plain text body
+                html: `<b>${this.state.message}</b>` // html body
+            });
+
+            console.log('Message sent: %s', info.messageId);
+            // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+            // Preview only available when sending through an Ethereal account
+            console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+            // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+        }
+
+        main().catch(console.error);
+    }
     render() {
         const siteTitle = "Edith Emmings"
         const siteDescription = "Edith Emmings Software Engineer Portfolio"
@@ -55,14 +97,14 @@ class HomeIndex extends React.Component {
                         <header className="major">
                             <h2>About</h2>
                         </header>
-                        <p>Accumsan orci faucibus id eu lorem semper. Eu ac iaculis ac nunc nisi 
-                            lorem vulputate lorem neque cubilia ac in adipiscing in curae lobortis 
-                            tortor primis integer massa adipiscing id nisi accumsan pellentesque 
-                            commodo blandit enim arcu non at amet id arcu magna. Accumsan orci faucibus 
+                        <p>Accumsan orci faucibus id eu lorem semper. Eu ac iaculis ac nunc nisi
+                            lorem vulputate lorem neque cubilia ac in adipiscing in curae lobortis
+                            tortor primis integer massa adipiscing id nisi accumsan pellentesque
+                            commodo blandit enim arcu non at amet id arcu magna. Accumsan orci faucibus
                             id eu lorem semper nunc nisi lorem vulputate lorem neque cubilia.</p>
-                        <ul className="actions">
+                        {/* <ul className="actions">
                             <li><a href="#" className="button">Learn More</a></li>
-                        </ul>
+                        </ul> */}
                     </section>
 
                     <section id="two">
@@ -90,23 +132,22 @@ class HomeIndex extends React.Component {
                         </ul>
                     </section>
 
-                        <section id="three">
-                            <h2>Get In Touch</h2>
-                            <p>Accumsan pellentesque commodo blandit enim arcu non at amet id arcu magna. Accumsan orci faucibus id eu lorem semper nunc nisi lorem vulputate lorem neque lorem ipsum dolor.</p>
-                            <div className="row">
-                                <div className="8u 12u$(small)">
-                                    <form method="post" action="#">
-                                        <div className="row uniform 50%">
-                                            <div className="6u 12u$(xsmall)"><input type="text" name="name" id="name" placeholder="Name" /></div>
-                                            <div className="6u 12u$(xsmall)"><input type="email" name="email" id="email" placeholder="Email" /></div>
-                                            <div className="12u"><textarea name="message" id="message" placeholder="Message" rows="4"></textarea></div>
-                                        </div>
-                                    </form>
-                                    <ul className="actions">
-                                        <li><input type="submit" value="Send Message" /></li>
-                                    </ul>
-                                </div>
-                                <div className="4u 12u$(small)">
+                    <section id="three">
+                        <h2>Get In Touch</h2>
+                        <div className="row">
+                            <div className="8u 12u$(small)">
+                                <form method="post" action="#">
+                                    <div className="row uniform 50%">
+                                        <div className="6u 12u$(xsmall)"><input onChange={(e) => this.handleChange(e, 'name')} type="text" name="name" id="name" placeholder="Name" /></div>
+                                        <div className="6u 12u$(xsmall)"><input onChange={(e) => this.handleChange(e, 'email')} type="email" name="email" id="email" placeholder="Email" /></div>
+                                        <div className="12u"><textarea onChange={(e) => this.handleChange(e, 'message')} name="message" id="message" placeholder="Message" rows="4"></textarea></div>
+                                    </div>
+                                </form>
+                                <ul className="actions">
+                                    <li><input type="submit" value="Send Message" /></li>
+                                </ul>
+                            </div>
+                            {/* <div className="4u 12u$(small)">
                                     <ul className="labeled-icons">
                                         <li>
                                             <h3 className="icon fa-home"><span className="label">Address</span></h3>
@@ -123,15 +164,15 @@ class HomeIndex extends React.Component {
                                             <a href="#">hello@untitled.tld</a>
                                         </li>
                                     </ul>
-                                </div>
-                            </div>
-                        </section>
+                                </div> */}
+                        </div>
+                    </section>
 
                 </div>
 
             </Layout>
-                )
-            }
-        }
-        
+        )
+    }
+}
+
 export default HomeIndex
